f01a4521f167a4f279427d1d5913b72d8037f256
SwetaPrasaddd <swetaprasad652@gmail.com>
2024-07-18 01:00:46 +0530

    Ep-3

--- App.js
+++ App.js
@@ -1,49 +1,62 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World");
-// console.log(heading); //OBJECT
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const heading=React.createElement("h1",{id:heading},"Namaste React");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-//NESTED STRUCTURE
-/* <div id="parent">
-  <div id="child">
-    <h1>I'm H1 tag</h1>
-  </div>
-</div>; */
 
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "I'm H1 tag")
-//   )
-// );
-// console.log(parent);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
 
-//SIBLING'S STRUCTURE
-/* <div id="parent">
-  <div id="child">
-    <h1>I'm H1 tag</h1>
-    <h2>I'm H2 tag</h2>
-  </div>
-</div>; */
+//using  jsx (react element)
+// const jsxHeading=<h1 id="heading" className="head">Namaste React using jsx</h1>
+// console.log(jsxHeading)
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag"),
-  ])
-);
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-//JSX is used to improve the above code mess
+
+//React functional component
+// const HeadingComponent=()=>{
+//     return (<h1 className="heading">
+//         Namste react functional component
+//     </h1>);
+// };
+
+//Component composition
+// const Title=()=>(
+//     <h1 className="head">Namste react</h1>
+//     );
+
+// const HeadingComponent=()=>(
+//     <div id="container">
+//         <Title/>
+//     <h1 className="heading">Namste react functional component</h1>
+//     </div>
+//     );
+//     // console.log(jsxHeading)
+//     const root=ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(<HeadingComponent/>);
+
+
+const elem=<span>React element</span>
+
+const number=10000;
+const HeadingComponent=()=>(
+        <div id="container">
+        {number}
+        <h2>{number}</h2>
+        <h2>{100+23}</h2>
+        <h1 className="heading">Namste react functional component</h1>
+        <h2>{console.log("sweta")}</h2> 
+        {title} //React element inside component
+        </div>
+        );
+
+    const title=(
+            <h1 className="head">
+                {elem}react element here
+            {/* <HeadingComponent/> */}
+            </h1>
+            );
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(title);
